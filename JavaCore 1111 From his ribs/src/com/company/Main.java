5cbcafff783515c766912364ce41787402155137
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-28 09:10:04 +0200

    Add files via upload

---
+++ JavaCore 1111 From his ribs/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+/*
+
+1111 From his ribs
+Change the two classes of Adam (Adam) and Eve (Eve).
+Inherit Eve from Adam.
+Requirements:
+1. There must be an Adam class in the Solution class.
+2. The Adam class must be public.
+3. In the class Solution there must be a class Eve (Eve).
+4. The Eve class must be public.
+5. The Eve class must be inherited from the Adam class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    // Adam
+    public class Adam {
+
+    }
+    // Eve
+    public class Eve extends Adam {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
